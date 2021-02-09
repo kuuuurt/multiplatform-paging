83d3b558aad725939f34834ba1ce8d4f004cfee8
Kurt Renzo Acosta <18444033+kuuuurt@users.noreply.github.com>
2021-02-10 01:42:10 +0800

    Adds  to  in iOS to determine if there are still more pages to be loaded

--- RELEASE.md
+++ RELEASE.md
@@ -1,2 +1,3 @@
 ## [0.3.5] - 2021-02-08
 ### What's new
+- Adds `hasNextPage` to `Pager` in iOS to determine if there are still more pages to be loaded
--- paging/src/iosMain/kotlin/com/kuuurt/paging/multiplatform/Pager.kt
+++ paging/src/iosMain/kotlin/com/kuuurt/paging/multiplatform/Pager.kt
@@ -5,6 +5,9 @@ import com.kuuurt.paging.multiplatform.helpers.asCommonFlow
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.ConflatedBroadcastChannel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.launch
 
 /**
@@ -23,10 +26,14 @@ actual class Pager<K : Any, V : Any> actual constructor(
 ) {
     private val items = PagingData<V>()
 
-    private val _pagingData = ConflatedBroadcastChannel<PagingData<V>>()
-    actual val pagingData: CommonFlow<PagingData<V>> get() = _pagingData.asCommonFlow()
+    private val _pagingData = MutableStateFlow<PagingData<V>?>(null)
+    actual val pagingData: CommonFlow<PagingData<V>> get() = _pagingData.filterNotNull().asCommonFlow()
+
+    private val _hasNextPage = MutableStateFlow(true)
+    val hasNextPage: CommonFlow<Boolean> get() = _hasNextPage.asCommonFlow()
 
     private var currentPagingResult: PagingResult<K, V>? = null
+
 
     init {
         loadNext()
@@ -58,7 +65,10 @@ actual class Pager<K : Any, V : Any> actual constructor(
             clientScope.launch {
                 val newPagingResult = getItems(key, config.pageSize)
                 items.addAll(newPagingResult.items)
-                _pagingData.offer(items)
+
+                _hasNextPage.value = newPagingResult.items.size < config.pageSize
+                _pagingData.value = items
+
                 currentPagingResult = newPagingResult
             }
         }
