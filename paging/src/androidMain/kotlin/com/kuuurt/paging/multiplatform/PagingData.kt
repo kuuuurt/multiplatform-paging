a476030ff23ac7bc092d72406d4d3cc331a10ab6
popalay <popalay1@gmail.com>
2021-06-16 17:53:01 +0300

    Add transform methods such as map and flatMap for PagingData

--- paging/src/androidMain/kotlin/com/kuuurt/paging/multiplatform/PagingData.kt
+++ paging/src/androidMain/kotlin/com/kuuurt/paging/multiplatform/PagingData.kt
@@ -1,6 +1,8 @@
 package com.kuuurt.paging.multiplatform
 
 import androidx.paging.filter
+import androidx.paging.flatMap
+import androidx.paging.map
 import androidx.paging.PagingData as AndroidXPagingData
 
 /**
@@ -12,6 +14,14 @@ import androidx.paging.PagingData as AndroidXPagingData
 
 actual typealias PagingData<T> = AndroidXPagingData<T>
 
-actual suspend fun <T: Any> PagingData<T>.filter(predicate: suspend (T) -> Boolean): PagingData<T> {
+actual suspend fun <T : Any> PagingData<T>.filter(predicate: suspend (T) -> Boolean): PagingData<T> {
     return this.filter(predicate)
+}
+
+actual suspend fun <T : Any, R : Any> PagingData<T>.map(transform: suspend (T) -> R): PagingData<R> {
+    return this.map(transform)
+}
+
+actual suspend fun <T : Any, R : Any> PagingData<T>.flatMap(transform: suspend (T) -> Iterable<R>): PagingData<R> {
+    return this.flatMap(transform)
 }
